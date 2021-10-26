d3cc0b7ce2b135948cdf3992e421a9e6992d3920
Malsha Sudusinghe <2019cs090@stu.ucsc.lk>
2021-10-26 12:32:54 +0530

    Create addCategoryHandle.php

---
+++ Implementation/backend/addCategoryHandle.php
@@ -0,0 +1,13 @@
+<?php
+require_once("dbconfig.php");
+if(isset($_POST['add'])){
+    $name = $_POST['categoryName'];
+    $adminId = 1;
+    $sql = "INSERT INTO categories(Name,Admin_ID) VALUES ('$name',$adminId)";
+    mysqli_query($connection,$sql);
+    header("location:../AddCategory.php?messege=1");
+    exit();
+}else{
+    header('location:/WADAK.com/Implementation/view/home.php');
+}
+?>
