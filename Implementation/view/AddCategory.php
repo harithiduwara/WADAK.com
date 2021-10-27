906e5f90c1d9a805d1ee601758337b3da118fc1c
Lasanthi Wathsala <lasanthiwathsala473@gmail.com>
2021-10-28 05:28:31 +0530

    Update AddCategory.php

--- Implementation/view/AddCategory.php
+++ Implementation/view/AddCategory.php
@@ -15,7 +15,7 @@ session_start();
 <div id="mySidenav" class="sidenav">
     <p class="logo">WADAK <span class="menu">&#9776;</span></p>
     <p class="logo1"> <span class="menu1">&#9776;</span></p>
-    <a href="home.php"class="icon-a"><i class="fa fa-home icons"></i> &nbsp;&nbsp;Home</a>
+    <a href="#"class="icon-a"><i class="fa fa-home icons"></i> &nbsp;&nbsp;Home</a>
     <a href="AdminPanel.html" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
     <a href="CategoryUI.php"class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Categories</a>
     <a href="#"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Co-Admin</a>
