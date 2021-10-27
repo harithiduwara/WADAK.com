8ae6fe59c08212e860b59866a7446c3ae3d981ff
Lasanthi Wathsala <lasanthiwathsala473@gmail.com>
2021-10-28 05:28:27 +0530

    Create addcategoryHandle.php

---
+++ Implementation/view/addcategoryHandle.php
@@ -0,0 +1,17 @@
+<?php
+require_once("dbconfig.php");
+if(isset($_POST['add'])){
+    $name = $_POST['achievementName'];
+    $place = $_POST['place'];
+    $date = $_POST['date'];
+    $adminId = 1;
+    $sql = "INSERT INTO achievements(Name,Place,Date,Admin_ID) VALUES ('$name','$place','$date',$adminId)";
+    mysqli_query($connection,$sql);
+    //header("Implementation\view\addac.php?messege=1");
+    header('location:/WADAK.com/Implementation/view/achievemts.php');
+    exit();
+}else{
+    header('location:/WADAK.com/Implementation/view/home.php');
+}
+?>
+
