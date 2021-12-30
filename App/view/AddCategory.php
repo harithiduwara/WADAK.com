718973b44845b67c7f02d8133eb838754573ae94
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2021-12-30 13:58:37 +0530

    Update AddCategory.php

--- App/view/AddCategory.php
+++ App/view/AddCategory.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <title>Add Category</title>
     <style>a {text-decoration: none;}</style>
-    <link rel="stylesheet" href="/WADAK.com/Implemenation/assets/css/AddCategory.css" type="text/css"/>
+    <link rel="stylesheet" href="/WADAK.com/App/assets/css/AddCategory.css" type="text/css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
@@ -63,7 +63,7 @@ session_start();
 
     <div class="inputContainer" style="margin:auto;width: 50%;">
         <div class="form" style="position: relative;">
-            <form action="backend/addCategoryHandle.php" method="POST">
+            <form action="#" method="POST">
                 <div class="inputbox">
                     <input type="text" name="categoryName" required="true" placeholder="Category Name">
                 </div>
