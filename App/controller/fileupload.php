a3c8259593250f1c67f31b8306d0977c05226032
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2022-01-01 17:28:29 +0530

    Update fileupload.php

--- App/controller/fileupload.php
+++ App/controller/fileupload.php
@@ -3,4 +3,8 @@
 target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadok = 1;
-$imageFileType 
+$imageFileType = strtolower(pathunfo($target_file, PATHINFO_EXTENSION));
+//Check if image file is a actual image or not 
+if(isset($_POST["submit"])){
+    
+}
