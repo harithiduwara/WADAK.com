0e54f8ba61abc3071f207eb7e4c2c21c095fdc39
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2022-01-06 18:12:20 +0530

    jobs

--- App/view/jobs.php
+++ App/view/jobs.php
@@ -48,7 +48,7 @@ $data = mysqli_query($con, "select * from postjob , register where register.uid 
                     <?php
                                 }
                                 ?>
-                    <!-- <li class="greenbar"><a href="jobs.php">Jobs</a></li> -->
+
                     <li><a href="/WADAK.com/App/view/services.php">Services</a></li>
 
                     <?php if(!isset($_SESSION["user"]["userrole"])){?>
@@ -131,92 +131,14 @@ $data = mysqli_query($con, "select * from postjob , register where register.uid 
                 </div>
             </div>
 
-            <?php } } else { echo "0 results" ; } ?>
-
-            <!-- <div class=" column">
-                                <div class="card1"><img src="download.jpg" alt="service"
-                                        style="width:100%; height:200px">
-                                    <h9 style=" margin-right:1000px">@username</h9>
-                                    <h3>Title</h3><br>
-                                    <p>Need a developer code in c or c++ and build applications</p><br>
-                                    <p><button style="background-color:green;"><i class="fa fa-heart"
-                                                style="margin-left:200px;"></i></button></p>
-                                </div>
-                </div>
-
-                <div class="column">
-                    <div class="card1"><img src="database.jpg" alt="service" style="width:100%; height:200px">
-                        <h9 style=" margin-right:1000px">@username</h9>
-                        <h3>Title</h3><br>
-                        <p>Need a databases erp CRM or management system developer</p><br>
-                        <p><button style="background-color:green;"><i class="fa fa-heart"
-                                    style="margin-left:200px;"></i></button></p>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="card1"><img src="database.jpg" alt="service" style="width:100%; height:200px">
-                        <h9 style=" margin-right:1000px">@username</h9>
-                        <h3>Title</h3><br>
-                        <p>Need a databases erp CRM or management systems</p><br>
-                        <p><button style="background-color:green;"><i class="fa fa-heart"
-                                    style="margin-left:200px;"></i></button></p>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div class="footer">
-                <div class="container">
-                    <div class="col1">
-                        <a href="#""><img class=" wadaklogo" src="/WADAK.com/App/assets/images/wadak.gif"
-                            alt="wadak gif"></a>
-                        <ul>
-                            <li><a href="#">Terms & Condition</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Terms & Condition</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col2">
-                        <ul>
-                            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-                            <li><i class="fab fa-instagram-square"></i></li>
-                            <li><i class="fab fa-linkedin"></i></li>
-                            <li><i class="fab fa-twitter-square"></i></li>
-                        </ul>
-
-                    </div>
-
-                    <div class="col3">
-                        <h1>Stay Connected!</h1>
-                        <p>Subscribe for the news letter</p>
-
-                        <form action="#" class="formnews">
-                            <label for="email">Email</label><br>
-                            <input type="email" id="email" name="email"><br>
-                            <label for="fname">First Name</label><br>
-                            <input type="text" id="fname" name="fname"><br>
-                            <label for="lname">Last Name</label><br>
-                            <input type="text" id="lname" name="lname"><br>
-                            <input id="submitnews" type="submit" value="Submit">
-                        </form>
-                    </div>
-
-                </div>
-                <div class="copyrights">
-                    <p><a href="#" target="_blank">Powered by Group18</a></p>
-                </div>
-
-            </div>
-
-            <!------------------x--------------footer----------------------x---------------->
-
-
-
-
+            <?php 
+            } 
+            } 
+            
+            else { 
+                echo "0 results" ; 
+                } 
+            ?>
 
 
             <script src="./js/Jquery3.4.1.min.js"></script>
