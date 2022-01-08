0155d41cc1a0f2675313f2b9c899dfb0204b1797
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-01-08 22:57:23 +0530

    Update CategoryUI.php

--- App/view/CategoryUI.php
+++ App/view/CategoryUI.php
@@ -24,7 +24,7 @@
             <a href="Admin-dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i>
                 &nbsp;&nbsp;Dashboard</a>
             <a href="CategoryUI.php" class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Categories</a>
-            <a href="/WADAK.com/Implementation/view/coadmin.php" class="icon-a"><i class="fa fa-users icons"></i>
+            <a href="/WADAK.com/App/view/coadmin.php" class="icon-a"><i class="fa fa-users icons"></i>
                 &nbsp;&nbsp;Co-Admin</a>
             <a href="/WADAK.com/Implementation/view/Add Coadmin.php" class="icon-a"><i class="fa fa-users icons"></i>
                 &nbsp;&nbsp;Add Co-Admin</a>
