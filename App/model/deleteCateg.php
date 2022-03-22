ea1171f0a10e93c5f8df581ee1e85e36aa4d2f75
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-03-22 15:03:55 +0530

    Update deleteCateg.php

--- App/model/deleteCateg.php
+++ App/model/deleteCateg.php
@@ -3,9 +3,11 @@ require 'cataconfig.php';
 
 if (isset($_GET['id'])) {
 	$id = $_GET['id'];
-	mysqli_query($wadak, "DELETE FROM categories WHERE C_ID = $id");
-	$_SESSION['message'] = "record deleted!"; 
-	header('location: /WADAK.com/App/view/news.php');
+    $sql = "DELETE FROM categories WHERE C_ID = $id";
+    mysqli_query($connection,$sql);
+    header("location:/WADAK.com/App/view/CategoryUI.php");
+    exit();
+
 }
 
 ?>
