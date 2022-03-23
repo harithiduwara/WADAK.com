1ea28ad87bdd576563d955ed4c0c21d64ece0547
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2022-03-24 00:45:00 +0530

    up

--- App/view/ViewCoAdmin.php
+++ App/view/ViewCoAdmin.php
@@ -4,9 +4,9 @@
     <head>
         <title>View Co-admin </title>
         <style>
-            a {
-                text-decoration: none;
-            }
+        a {
+            text-decoration: none;
+        }
 
         </style>
         <link rel="stylesheet" href="/WADAK.com/App/assets/css/ViewServiceProvider2-coadmin.css" type="text/css" />
@@ -22,15 +22,20 @@
             <a href="/WADAK.com/App/view/home.php" class="icon-a"><i class="fa fa-home icons"></i> &nbsp;&nbsp;Home</a>
             <a href="/WADAK.com/App/view/Admin-dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i>
                 &nbsp;&nbsp;Dashboard</a>
-            <a href="/WADAK.com/App/view/CategoryUI.php" class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Categories</a>
-            <a href="/WADAK.com/App/view/coadmin.php" class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Co-Admin</a>
-            <a href="/WADAK.com/App/view/ServiceProviderUI-admin.php" class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Service
+            <a href="/WADAK.com/App/view/CategoryUI.php" class="icon-a"><i class="fa fa-tasks icons"></i>
+                &nbsp;&nbsp;Categories</a>
+            <a href="/WADAK.com/App/view/coadmin.php" class="icon-a"><i class="fa fa-users icons"></i>
+                &nbsp;&nbsp;Co-Admin</a>
+            <a href="/WADAK.com/App/view/ServiceProviderUI-admin.php" class="icon-a"><i class="fa fa-users icons"></i>
+                &nbsp;&nbsp;Service
                 Provider</a>
             <a href="#" class="icon-a"><i class="fa fa-bullhorn icons"></i> &nbsp;&nbsp;Advertisements</a>
-            <a href="/WADAK.com/App/view/View Report.php" class="icon-a"><i class="fa fa-envelope icons"></i> &nbsp;&nbsp;Reports</a>
-            <a href="/WADAK.com/App/view/PaymentUI.php" class="icon-a"><i class="fa fa-money icons"></i> &nbsp;&nbsp;Payments</a>
+            <a href="/WADAK.com/App/view/View Report.php" class="icon-a"><i class="fa fa-envelope icons"></i>
+                &nbsp;&nbsp;Reports</a>
+            <a href="/WADAK.com/App/view/PaymentUI.php" class="icon-a"><i class="fa fa-money icons"></i>
+                &nbsp;&nbsp;Payments</a>
             <a href="#" class="icon-a"><i class="fa fa-object-group icons"></i> &nbsp;&nbsp;Request</a>
-            <!--<a href="#"class="icon-a"><i class="fa fa-bell icons"></i> &nbsp;&nbsp;Notification</a>-->
+
         </div>
 
         <div id="main">
@@ -70,10 +75,10 @@
             <br />
 
             <div class="search-form">
-                    <form action="" method="POST" >
-                        <input type ="text" name ="uid" placeholder="Enter User Id" class="text">
-                        <input type ="submit" name ="search" placeholder="Search" value="Search" class="submit">
-                    </form>
+                <form action="" method="POST">
+                    <input type="text" name="uid" placeholder="Enter User Id" class="text">
+                    <input type="submit" name="search" placeholder="Search" value="Search" class="submit">
+                </form>
             </div>
 
             <?php 
@@ -88,74 +93,74 @@ if(isset($_POST['search']))
     while($row = mysqli_fetch_array($query_run))
     {
         ?>
-  <div class="col-div-2">
-    <div class="box">
-      
-        <table>
-          <tr>
-            <td>
-               Name :
-            </td>
-            <td>
-              <?php echo $row['name']; ?>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Co-admin ID :
-            </td>
-            <td>
-              <?php echo $row['uid']; ?>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Contact :
-            </td>
-            <td>
-               <?php echo $row['contactno']; ?>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Email :
-            </td>
-            <td>
-              <?php echo $row['email']; ?>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Address:
-            </td>
-            <td>
-              <?php echo $row['address']; ?> 
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Date of Birth :
-            </td>
-            <td>
-              <?php echo $row['birthday']; ?>
-            </td>
-          </tr>
+            <div class="col-div-2">
+                <div class="box">
 
-        </table>
+                    <table>
+                        <tr>
+                            <td>
+                                Name :
+                            </td>
+                            <td>
+                                <?php echo $row['name']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Co-admin ID :
+                            </td>
+                            <td>
+                                <?php echo $row['uid']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Contact :
+                            </td>
+                            <td>
+                                <?php echo $row['contactno']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Email :
+                            </td>
+                            <td>
+                                <?php echo $row['email']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Address:
+                            </td>
+                            <td>
+                                <?php echo $row['address']; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Date of Birth :
+                            </td>
+                            <td>
+                                <?php echo $row['birthday']; ?>
+                            </td>
+                        </tr>
 
-      </form>
-      
-    </div>
-  </div>
-  <div class="col-div-2">
-    <div class="imgbox">
-      <div class="img">
-        <img src="/WADAK.com/App/assets/images/sp.png" width="325" height="380">
-      </div>
-      
-    </div>
+                    </table>
 
-  <?php
+                    </form>
+
+                </div>
+            </div>
+            <div class="col-div-2">
+                <div class="imgbox">
+                    <div class="img">
+                        <img src="/WADAK.com/App/assets/images/sp.png" width="325" height="380">
+                    </div>
+
+                </div>
+
+                <?php
     }
     if($row==0)
      {
@@ -163,50 +168,48 @@ if(isset($_POST['search']))
      }
 }
 ?>
-      
 
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script>
 
-            $(".menu").click(function () {
-                $("#mySidenav").css('width', '70px');
-                $("#main").css('margin-left', '70px');
-                $(".logo").css('display', 'none');
-                $(".logo1").css('display', 'block');
-                $(".logo span").css('visibility', 'visible');
-                $(".logo span").css('margin-left', '-10px');
-                $(".icon-a").css('visibility', 'hidden');
-                $(".icon-a").css('height', '25px');
-                $(".icons").css('visibility', 'visible');
-                $(".icons").css('margin-left', '-8px');
-                $(".menu1").css('display', 'block');
-                $(".menu").css('display', 'none');
-            });
-
-            $(".menu1").click(function () {
-                $("#mySidenav").css('width', '300px');
-                $("#main").css('margin-left', '300px');
-                $(".logo").css('visibility', 'visible');
-                $(".logo").css('display', 'block');
-                $(".icon-a").css('visibility', 'visible');
-                $(".icons").css('visibility', 'visible');
-                $(".menu").css('display', 'block');
-                $(".menu1").css('display', 'none');
-            });
-
-        </script>
-        <script>
-            $(document).ready(function () {
-                $(".profile p").click(function () {
-                    $(".profile-div").toggle();
-
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script>
+                $(".menu").click(function() {
+                    $("#mySidenav").css('width', '70px');
+                    $("#main").css('margin-left', '70px');
+                    $(".logo").css('display', 'none');
+                    $(".logo1").css('display', 'block');
+                    $(".logo span").css('visibility', 'visible');
+                    $(".logo span").css('margin-left', '-10px');
+                    $(".icon-a").css('visibility', 'hidden');
+                    $(".icon-a").css('height', '25px');
+                    $(".icons").css('visibility', 'visible');
+                    $(".icons").css('margin-left', '-8px');
+                    $(".menu1").css('display', 'block');
+                    $(".menu").css('display', 'none');
                 });
-                $(".noti-icon").click(function () {
-                    $(".notification-div").toggle();
+
+                $(".menu1").click(function() {
+                    $("#mySidenav").css('width', '300px');
+                    $("#main").css('margin-left', '300px');
+                    $(".logo").css('visibility', 'visible');
+                    $(".logo").css('display', 'block');
+                    $(".icon-a").css('visibility', 'visible');
+                    $(".icons").css('visibility', 'visible');
+                    $(".menu").css('display', 'block');
+                    $(".menu1").css('display', 'none');
                 });
-            });
-        </script>
+                </script>
+                <script>
+                $(document).ready(function() {
+                    $(".profile p").click(function() {
+                        $(".profile-div").toggle();
+
+                    });
+                    $(".noti-icon").click(function() {
+                        $(".notification-div").toggle();
+                    });
+                });
+                </script>
     </body>
 
 </html>
--- App/view/view service providers- hire person.php
+++ App/view/view service providers- hire person.php
@@ -1,88 +1,111 @@
 <!DOCTYPE html>
 <html>
+
     <head>
         <title>
             view service providers-hire person
         </title>
         <link rel="stylesheet" href="/WADAK.com/Implementation/assets/css/view service providers- hire person.css">
     </head>
+
     <body>
 
         <nav>
             <label class="logo">WADAK</label>
-            <ul>
-                <li><a href="/WADAK.com/App/view/home.php" class="active">Home</a></li>
-                <li><a href="#">Jobs</a> </li>
-                <li><a href="/WADAK.com/App/view/Add achievement post - Service provider.php">POST JOB</a> </li>
-                <li><a href="#">Services</a> </li>
-                <li><a href="/WADAK.com/App/view/leaderboard.php">Leaderboard</a></li>
-                <li><a href="/WADAK.com/App/view/chat.php">MESSAGES</a> </li>
-                <li><a href="/WADAK.com/implementation/controller/logout.php">Logout</a> </li>
-                <div class="animation "></div>
+            <ul style="margin-top: 1rem">
+                <?php
+                        if(isset($_SESSION["user"]["userrole"])){?>
+                <li><a href="/WADAK.com/App/view/postjob.php">Add Post</a></li>
+                <?php
+                    }
+                    ?>
+                <?php 
+                    if($postType=="job"){?>
+                <li><a href="/WADAK.com/App/view/jobs.php?postType=service">Services</a></li>
+                <?php }
+                    else{ ?>
+                <li><a href="/WADAK.com/App/view/jobs.php?postType=job">Jobs</a></li>
+                <?php }
+                    ?>
+
+
+                <?php if(!isset($_SESSION["user"]["userrole"])){?>
+
+                <li><a href="./login.php">Login</a></li>
+                <?php }else {?>
+                <li><a href="/WADAK.com/App/view/messages.php">Messages</a></li>
+                <li><a href="/WADAK.com/App/view/hirepersondashboard.php"><i class="fas fa-user"></i></a></li>
+                <?php } ?>
+
+
             </ul>
         </nav>
 
         <!--1st container-->
         <div class="container">
             <div class="wrapper">
-              
-              <form>
-                <img src="programmer.png" alt="user image" class="right" style="width:50px;height:50px; align-items:center;">
-                <div class="row">
-                  <span>Name </span><br><br>
-                  <span>Category </span><br><br>
-                  <span>Telephone Number </span><br><br>
-                </div>
-      
-                <div class="buttons">
-                  <br> <button class="button button1"> <a href="user profile.html" >View Profile</button> </a>   
-                </div>
-                
-            </div>
-            </form>
-          </div>
 
-           <!--2 container-->
-          <div class="container">
-            <div class="wrapper">
-              
-              <form>
-                <img src="girl.png" alt="user image" class="right" style="width:50px;height:50px; align-items:center;">
-                <div class="row">
-                  <span>Name </span><br><br>
-                  <span>Category </span><br><br>
-                  <span>Telephone Number </span><br><br>
-                </div>
-      
-                <div class="buttons">
-                  <br> <button class="button button1"><a href="user profile.html" >View Profile</button></a>
-                  
-                </div>
-                
-            </div>
-            </form>
-          </div>
+                <form>
+                    <img src="programmer.png" alt="user image" class="right"
+                        style="width:50px;height:50px; align-items:center;">
+                    <div class="row">
+                        <span>Name </span><br><br>
+                        <span>Category </span><br><br>
+                        <span>Telephone Number </span><br><br>
+                    </div>
 
-           <!--3 container-->
-           <div class="container">
-            <div class="wrapper">
-              
-              <form>
-                <img src="programmer.png" alt="user image" class="right" style="width:50px;height:50px; align-items:center;">
-                <div class="row">
-                  <span>Name </span><br><br>
-                  <span>Category </span><br><br>
-                  <span>Telephone Number </span><br><br>
-                </div>
-      
-                <div class="buttons">
-                  <br> <button class="button button1"><a href="user profile.html" >View Profile</button></a>
-                  
-                </div>
-                
+                    <div class="buttons">
+                        <br> <button class="button button1"> <a href="user profile.html">View Profile</button> </a>
+                    </div>
+
             </div>
             </form>
-          </div>
+        </div>
+
+        <!--2 container-->
+        <div class="container">
+            <div class="wrapper">
+
+                <form>
+                    <img src="girl.png" alt="user image" class="right"
+                        style="width:50px;height:50px; align-items:center;">
+                    <div class="row">
+                        <span>Name </span><br><br>
+                        <span>Category </span><br><br>
+                        <span>Telephone Number </span><br><br>
+                    </div>
+
+                    <div class="buttons">
+                        <br> <button class="button button1"><a href="user profile.html">View Profile</button></a>
+
+                    </div>
+
+            </div>
+            </form>
+        </div>
+
+        <!--3 container-->
+        <div class="container">
+            <div class="wrapper">
+
+                <form>
+                    <img src="programmer.png" alt="user image" class="right"
+                        style="width:50px;height:50px; align-items:center;">
+                    <div class="row">
+                        <span>Name </span><br><br>
+                        <span>Category </span><br><br>
+                        <span>Telephone Number </span><br><br>
+                    </div>
+
+                    <div class="buttons">
+                        <br> <button class="button button1"><a href="user profile.html">View Profile</button></a>
+
+                    </div>
+
+            </div>
+            </form>
+        </div>
 
     </body>
-    </html>    
+
+</html>
