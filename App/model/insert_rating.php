7362d01d882c3650a8983f91b862a4b2691b0a1a
hansika123 <hansikak1998@gmail.com>
2022-03-24 13:31:59 +0530

    up

--- App/model/insert_rating.php
+++ App/model/insert_rating.php
@@ -1,20 +1,20 @@
 <?php
 require 'newsconfig.php';
-header('content-Type:application/json');
-$json=[];
-try{
-    if(!isset($_POST['rating']) && empty($_POST['rating'])){
-        throw new Exception("data not insert");
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $name = $_POST["name"];
+    $rate = $_POST["rate"];
+    $comment=$_POST["comment"];
+ 
+    $sql = "INSERT INTO star_rating (name,rate,comment) VALUES ('$name','$rate','$comment')";
+    if (mysqli_query($conn, $sql))
+    {
+        echo "New Rate addedddd successfully";
     }
-    $query = $conn -> query("INSERT INTO star_rating VALUES('',".$_POST['rating'].")");
-    if($query){
-        $json=['success'=>TRUE,'message' =>'Insert data successfully'];
-    }else{
-        $json=['error'=>TRUE,'message' =>'data not insterted'];
+    else
+    {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-    }catch(Exception $e){
-        $json=['error'=>TRUE,'message' => $e->getMessage()];
-    }
-    echo json_encode($json);
-
+    mysqli_close($conn);
+}
 ?>
--- App/view/rating.php
+++ App/view/rating.php
@@ -1,41 +1,67 @@
 <!Doctype HTML>
 <html>
+<head>
+    <link rel="stylesheet" href="/WADAK.com/App/assets/css/rating.css" type="text/css" />
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+--><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+</head>
+<body>
 
-    <head>
-        <title>Rating</title>
-        <style>
-        a {
-            text-decoration: none;
-        }
 
-        </style>
-        <link rel="stylesheet" href="/WADAK.com/App/assets/css/rating.css" type="text/css" />
-        <link rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <meta charset="utf-8">
-        </head>
 
-    <body>
-        <form method="POST" name="form" id="form">
-            <p id ="msg"></p>
-        <fieldset class="rating">
-            <input type= "radio" id="star5"  name="rating" value="5"/><label class="full" for="star5" title=" 5 stars"></label>
-            <input type= "radio" id="star4half"  name="rating" value="4.5"/><label class="half" for="star4half" title=" 4.5 stars"></label>
-            <input type= "radio" id="star4"  name="rating" value="4"/><label class="full" for="star4" title="4 stars"></label>
-            <input type= "radio" id="star3half"  name="rating" value="3.5"/><label class="half" for="star3half" title="3.5 stars"></label>
-            <input type= "radio" id="star3"  name="rating" value="3"/><label class="full" for="star3" title=" 3 stars"></label>
-            <input type= "radio" id="star2half"  name="rating" value="2.5"/><label class="half" for="star2half" title="2.5 stars"></label>
-            <input type= "radio" id="star2"  name="rating" value="2"/><label class="full" for="star2" title="2 stars"></label>
-            <input type= "radio" id="star1half"  name="rating" value="1.5"/><label class="half" for="star1half" title="1.5 stars"></label>
-            <input type= "radio" id="star1"  name="rating" value="1"/><label class="full" for="star1" title="1 star"></label>
-            <input type= "radio" id="starhalf"  name="rating" value="0.5"/><label class="half" for="starhalf" title="0.5 stars"></label>
-    </fieldset>
-    </form>
+
+        
+    <form action="/WADAK.com/App/model/insert_rating.php" method="post">
+         <h4>Name</h4>
+        <input type="text" name="name">
     
-    <script src="http://ajax.googlepis.com/ajax/libs/jquery/1.11.0/jquery.min.js"> </script>
-    <script type="text/javascript">
-        $(document).ready(function)
-    </body>
-    </html>
+    <br>
+     
+         <h4>Comment</h4>
+        <input type="text" name="comment">
+        <br><br>
+    
+         <div class="rateyo" id= "rate"
+         data-rateyo-rating="4"
+         data-rateyo-num-stars="5"
+         data-rateyo-score="3">
+         </div>
+ 
+    <span class='result'>0</span>
+    <br>
+    <input type="hidden" name="rate">
+ 
+    
+   <br>
+    <input type="submit" name="add"> 
+    
+
+    
+    
+ 
+</form>
+                
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.js"></script>
+ 
+<script>
+ 
+ 
+    $(function () {
+        $(".rateyo").rateYo().on("rateyo.change", function (e, data) {
+            var rating = data.rating;
+            $(this).parent().find('.score').text('score :'+ $(this).attr('data-rateyo-score'));
+            $(this).parent().find('.result').text('rate :'+ rating);
+            $(this).parent().find('input[name=rate]').val(rating); //add rating value to input field
+        });
+    });
+ 
+</script>
+</body>
+ 
+</html>
+
+       
+        
 
             
