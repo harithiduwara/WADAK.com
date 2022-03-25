685f73015dfdf4106460dfef5f30472bd6bcb8ba
hansika123 <hansikak1998@gmail.com>
2022-03-26 01:05:28 +0530

    Update searchsp.php

--- App/model/searchsp.php
+++ App/model/searchsp.php
@@ -1,7 +1,7 @@
 <!--php code-->
 <?php
                   include 'newsconfig.php';
-                  $sql="SELECT * FROM service_provider WHERE SP_ID='".$_GET["User ID"] ."'";
+                  $sql="SELECT * FROM service_provider WHERE UserName='".$_GET["UserName"] ."'";
                   $result=mysqli_query($conn,$sql);
                   $row=mysqli_fetch_assoc($result);
 ?>
