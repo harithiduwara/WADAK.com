0809f4fca9fd3f7f52d3967f392f92a5a0795782
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-03-26 10:46:31 +0530

    Create request.php

---
+++ App/view/request.php
@@ -0,0 +1,18 @@
+<!Doctype HTML>
+<html>
+
+    <head>
+        <title>update request</title>
+        <style>
+        a {
+            text-decoration: none;
+        }
+
+        </style>
+        <link rel="stylesheet" href="/WADAK.com/App/assets/css/CategoryUI.css" type="text/css" />
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+
+</html>
