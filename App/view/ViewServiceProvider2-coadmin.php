9710cb158d41b512defd59944d49ccac10ba8f17
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2022-03-28 00:57:11 +0530

    jobs.php uupdated

--- App/view/ViewServiceProvider2-coadmin.php
+++ App/view/ViewServiceProvider2-coadmin.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+$postType = $_GET["postType"] ?? "job";
+
+$con = mysqli_connect('localhost', 'toor', 'toor', 'wadak');
+if (!$con) {
+    die("Connection failed" . mysqli_connect_error());
+}?>
+
 <!Doctype HTML>
 <html>
 <head>
@@ -67,8 +77,7 @@
   </div>
 
 <?php 
-$conn= mysqli_connect("localhost","root","");
-$db=mysqli_select_db($conn,'wadak');
+
 if(isset($_POST['search']))
 {
     $username= $_POST['username'];
