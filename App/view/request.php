f64118e52eab0eeee1c79e36ade6bb68eb617696
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-03-27 23:34:59 +0530

    Update request.php

--- App/view/request.php
+++ App/view/request.php
@@ -14,5 +14,21 @@
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
+    <body>
+
+                        <div class="col-md-7">
+
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search data">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+
+                            </div>
+
+    
+    </body>
+
 
 </html>
