7fc957ab9750b93155aee235d16d99033598d857
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-03-27 23:34:50 +0530

    Update ViewServiceProvider-admin.php

--- App/view/ViewServiceProvider-admin.php
+++ App/view/ViewServiceProvider-admin.php
@@ -15,7 +15,7 @@
     <a href="/WADAK.com/App/view/Admin-dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
     <a href="/WADAK.com/App/view/CategoryUI.php"class="icon-a"><i class="fa fa-tasks icons"></i> &nbsp;&nbsp;Categories</a>
     <a href="/WADAK.com/App/view/CoAdmin-profile.php"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Co-Admin</a>
-    <a href="/WADAK.com/App/view/ServiceProviderUI-admin.php"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Service Provider</a>
+    <a href="/WADAK.com/App/view/ServiceProviderUI-admin.php"class="icon-a"><i class="fa fa-users icons"></i> &nbsp;&nbsp;Users</a>
     <a href="#"class="icon-a"><i class="fa fa-bullhorn icons"></i> &nbsp;&nbsp;Advertisements</a>
     <a href="/WADAK.com/App/view/View Report.php"class="icon-a"><i class="fa fa-envelope icons"></i> &nbsp;&nbsp;Reports</a>
     <a href="/WADAK.com/App/view/PaymentUI.php"class="icon-a"><i class="fa fa-money icons"></i> &nbsp;&nbsp;Payments</a>
@@ -43,7 +43,7 @@
 
             <div class="profile">
             
-                <p>Admin Name <i class="fa fa-ellipsis-v dots" aria-hidden="true"></i></p>
+                <p>Admin <i class="fa fa-ellipsis-v dots" aria-hidden="true"></i></p>
                 <div class="profile-div">
                     <p><i class="fa fa-user"></i> &nbsp;&nbsp; <a href="AdminProfile.html">Profile</a></p>
                     <p><i class="fa fa-dashboard"></i> &nbsp;&nbsp; <a href="Admin-dashboard.html">Dashboard</a> </p>
