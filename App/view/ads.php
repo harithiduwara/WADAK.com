455fe57ad3bba4f1bdedd84909a6a7765d256c7e
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-03-28 04:04:19 +0530

    Update ads.php

--- App/view/ads.php
+++ App/view/ads.php
@@ -63,10 +63,10 @@
                     <div class="profile">
                         <p>Admin<i class="fa fa-ellipsis-v dots" aria-hidden="true"></i></p>
                         <div class="profile-div">
-                            <p><i class="fa fa-user"></i> &nbsp;&nbsp; <a href="#">Profile</a></p>
-                            <p><i class="fa fa-dashboard"></i> &nbsp;&nbsp; <a href="#">Dashboard</a> </p>
+                            
+                            <p><i class="fa fa-dashboard"></i> &nbsp;&nbsp; <a href="Admin-dashboard.php">Dashboard</a> </p>
                             <p><i class="fa fa-power-off"></i> &nbsp;&nbsp; <a
-                                    href="/WADAK.com/implementation/controller/logout.php">Log Out</a></p>
+                                    href="/WADAK.com/App/controller/logout.php">Log Out</a></p>
                         </div>
                     </div>
                 </div>
