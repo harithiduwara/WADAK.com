27fecb0b47f409f27e0630468eb1acca3e985d8d
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2022-03-28 06:05:36 +0530

    search done

--- App/view/messages.php
+++ App/view/messages.php
@@ -76,7 +76,7 @@ $search2 = mysqli_fetch_assoc($search1);
 
                 <?php
                 
-                $users_query = "SELECT * FROM register where ";
+                $users_query = "SELECT * FROM register where username like '%" . $_GET["search"] . "%' ";
 
                 $users = mysqli_query($con, $users_query);
 
